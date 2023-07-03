44119722cd9e45315094d5eb8647b57d994e91e6
Achintya <achintya22052000@gmail.com>
2023-07-03 16:19:31 +0530

    initial setup

--- vehicle-tracker/vehicle_tracker_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ vehicle-tracker/vehicle_tracker_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_secure_storage_macos
+import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
 }
