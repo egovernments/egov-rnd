8af69b483918b2715e9695a9d419f833af30ba93
Achintya <achintya22052000@gmail.com>
2023-07-14 21:15:05 +0530

    [chore] some code refactoring

--- vehicle-tracker/vehicle_tracker_app/lib/features/map/controllers/map_controllers.dart
+++ vehicle-tracker/vehicle_tracker_app/lib/features/map/controllers/map_controllers.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: avoid_print
-
 import 'dart:async';
-
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 
@@ -11,60 +9,37 @@ class MapControllers extends GetxController {
   late Position start;
   late Position end;
 
+  // * starts from here
+  void startTracking(Position start, end) {
+    this.start = start;
+    this.end = end;
+    isRunning.value = true;
+    startPeriodicFunction();
+  }
+
+  void stopTracking() {
+    // send completed status via API
+    isRunning.value = false;
+  }
+
   void startPeriodicFunction() {
     Timer.periodic(const Duration(minutes: 2), (_) async {
-      // * If isRunning is false, cancel the timer
+      // If isRunning is false, cancel the timer
       if (!isRunning.value) {
         _.cancel();
       } else {
         Position? currentPosition = await getCurrentLocation();
         if (currentPosition != null) {
-          // * Add the current position to the list
+          // Add the current position to the list
           positions.add(currentPosition);
 
-          // * If there are more than 1 positions, call the distance logic function
+          // If there are more than 1 positions, call the distance logic function
           if (positions.length > 1) {
             distanceLogic(currentPosition, positions[positions.length - 2]);
           }
         }
       }
     });
-  }
-
-  distanceLogic(Position currentPosition, prevPosition) {
-    double distance = distanceCalculator(currentPosition, prevPosition);
-    double distanceFromEnd = distanceCalculator(currentPosition, end);
-
-    if (distance < 2500) {
-      // Send slow report
-    }
-
-    if (distanceFromEnd < 2500) {
-      // Send near to destination report
-    }
-
-    if (distanceFromEnd < 500) {
-      // Send reached destination report
-    }
-  }
-
-  statusSender(String message) {
-    if (isConnected()) {
-      // send the status via API 
-      // if gets an error for any reason, store it in local db
-    } else {
-      // store it in local db
-    }
-  }
-
-  bool isConnected() {
-    // check Internet connection
-
-    return true;
-  }
-
-  double distanceCalculator(Position a, b) {
-    return Geolocator.distanceBetween(a.latitude, a.longitude, b.latitude, b.longitude);
   }
 
   Future<Position?> getCurrentLocation() async {
@@ -78,15 +53,43 @@ class MapControllers extends GetxController {
     }
   }
 
-  void startTracking(Position start, end) {
-    this.start = start;
-    this.end = end;
-    isRunning.value = true;
-    startPeriodicFunction();
+  distanceLogic(Position currentPosition, prevPosition) {
+    double distance = distanceCalculator(currentPosition, prevPosition);
+    double distanceFromEnd = distanceCalculator(currentPosition, end);
+
+    if (distanceFromEnd < 2500) {
+      // Send near to destination report
+    }
+
+    if (distanceFromEnd < 100) {
+      // Send reached destination report
+      stopTracking();
+    }
+
+    if (distance < 2500) {
+      // Send slow report
+    }
   }
 
-  void stopTracking() {
-    // send completed status via API
-    isRunning.value = false;
+  statusSender(String message) {
+    if (isConnected()) {
+      // send the status via API
+      // if gets an error for any reason, store it in local db
+    } else {
+      // store it in local db
+    }
   }
+
+  bool isConnected() {
+    // check Internet connection
+    return false;
+  }
+
+  double distanceCalculator(Position a, b) {
+    return Geolocator.distanceBetween(a.latitude, a.longitude, b.latitude, b.longitude);
+  }
+
+  
+
+  
 }
