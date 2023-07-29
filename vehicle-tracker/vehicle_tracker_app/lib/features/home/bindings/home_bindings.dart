938292f7aedee1eb63901dd4d7358f6d22c53487
Achintya <achintya22052000@gmail.com>
2023-07-29 15:53:15 +0530

    [clean] removed unwanted place controller

--- vehicle-tracker/vehicle_tracker_app/lib/features/home/bindings/home_bindings.dart
+++ vehicle-tracker/vehicle_tracker_app/lib/features/home/bindings/home_bindings.dart
@@ -1,11 +1,9 @@
 import 'package:get/get.dart';
-import 'package:vehicle_tracker_app/features/home/controllers/place_controller.dart';
 import 'package:vehicle_tracker_app/features/map/controllers/map_controllers.dart';
 
 class HomeBindings extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<PlaceController>(() => PlaceController());
     Get.lazyPut<MapControllers>(() => MapControllers());
   }
 }
