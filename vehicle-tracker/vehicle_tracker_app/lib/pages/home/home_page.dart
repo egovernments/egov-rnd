2f22dd1d2494ba175630761eca935e12765e48d6
Achintya <achintya22052000@gmail.com>
2023-08-15 02:58:45 +0530

    [UI] added Home page into safe area

--- vehicle-tracker/vehicle_tracker_app/lib/pages/home/home_page.dart
+++ vehicle-tracker/vehicle_tracker_app/lib/pages/home/home_page.dart
@@ -15,69 +15,71 @@ class HomePage extends StatelessWidget {
     final infoController = Get.find<InfoController>();
     final theme = DigitTheme.instance;
 
-    return Scaffold(
-      // * Appbar
-      appBar: AppBar(
-        centerTitle: false,
-        title: Text(AppTranslation.HOME_APP_BAR.tr),
-      ),
+    return SafeArea(
+      child: Scaffold(
+        // * Appbar
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text(AppTranslation.HOME_APP_BAR.tr),
+        ),
 
-      // * Dwawer
-      drawer: const CustomDrawer(),
+        // * Dwawer
+        drawer: const CustomDrawer(),
 
-      // * Body
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // * Help Button
-          Align(
-            alignment: Alignment.topRight,
-            child: DigitIconButton(
-              icon: Icons.help_outline_rounded,
-              iconText: AppTranslation.HELP.tr,
-              onPressed: () {},
-            ),
-          ),
-
-          // * App heading
-          Padding(
-            padding: theme.buttonPadding,
-            child: Text(
-              AppTranslation.MY_TRIPS.tr,
-              style: Theme.of(context).textTheme.displayMedium,
-            ),
-          ),
-
-          // * Digiit Search Bar
-          DigitSearchBar(
-            controller: infoController.searchController,
-            hintText: AppTranslation.SEARCH_BAR.tr,
-            onChanged: (value) {
-              infoController.isTextControllerEmpty.value = value.isEmpty;
-              infoController.onChangedFilter(value);
-            },
-          ),
-
-          const SizedBox(height: 8),
-
-          // * button bar for IN-PROGRESS and COMPLETED
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              DigitOutLineButton(
-                label: AppTranslation.IN_PROGRESS.tr,
-                onPressed: () => (),
+        // * Body
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // * Help Button
+            Align(
+              alignment: Alignment.topRight,
+              child: DigitIconButton(
+                icon: Icons.help_outline_rounded,
+                iconText: AppTranslation.HELP.tr,
+                onPressed: () {},
               ),
-              DigitOutLineButton(
-                label: AppTranslation.COMPLETED.tr,
-                onPressed: () => (),
-              ),
-            ],
-          ),
+            ),
 
-          // * trip list builder widget
-          TripInfoListBuilderWidget(infoController: infoController),
-        ],
+            // * App heading
+            Padding(
+              padding: theme.buttonPadding,
+              child: Text(
+                AppTranslation.MY_TRIPS.tr,
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+            ),
+
+            // * Digiit Search Bar
+            DigitSearchBar(
+              controller: infoController.searchController,
+              hintText: AppTranslation.SEARCH_BAR.tr,
+              onChanged: (value) {
+                infoController.isTextControllerEmpty.value = value.isEmpty;
+                infoController.onChangedFilter(value);
+              },
+            ),
+
+            const SizedBox(height: 8),
+
+            // * button bar for IN-PROGRESS and COMPLETED
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                DigitOutLineButton(
+                  label: AppTranslation.IN_PROGRESS.tr,
+                  onPressed: () => (),
+                ),
+                DigitOutLineButton(
+                  label: AppTranslation.COMPLETED.tr,
+                  onPressed: () => (),
+                ),
+              ],
+            ),
+
+            // * trip list builder widget
+            TripInfoListBuilderWidget(infoController: infoController),
+          ],
+        ),
       ),
     );
   }
