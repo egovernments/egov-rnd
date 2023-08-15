ca458bde5b672229b6e7801ad51ac389542d3023
Achintya <achintya22052000@gmail.com>
2023-08-15 12:38:17 +0530

    [fix] removed unwanted try catch bock

--- vehicle-tracker/vehicle_tracker_app/lib/blocs/home/repository/home_http_repository.dart
+++ vehicle-tracker/vehicle_tracker_app/lib/blocs/home/repository/home_http_repository.dart
@@ -5,6 +5,7 @@ import 'package:vehicle_tracker_app/constants.dart';
 import 'package:vehicle_tracker_app/data/http_service.dart';
 import 'package:vehicle_tracker_app/models/home_trip/home_trip_model/home_trip_model.dart';
 import 'package:vehicle_tracker_app/models/trip/trip_tracker_info/trip_tracker_hive_model.dart';
+import 'package:vehicle_tracker_app/util/toaster.dart';
 
 class HomeHTTPRepository {
   // ? Uses the userId to get the list of trips.
@@ -16,13 +17,8 @@ class HomeHTTPRepository {
 
     final response = await HttpService.getRequest(reqUrl);
 
-    if (response.statusCode == 500) {
-      log("Server Error: ${response.body}");
-      return homeTripModel;
-    }
-
     if (response.statusCode != 200) {
-      log("Error: ${response.body}");
+      toaster(Get.context, "Network or Server Error");
       return homeTripModel;
     }
 
@@ -56,20 +52,15 @@ class HomeHTTPRepository {
       "distanceMeters": 200000,
     };
 
-    try {
-      final response = await HttpService.postRequestWithoutToken(reqUrl, body);
+    final response = await HttpService.postRequestWithoutToken(reqUrl, body);
 
-      // ? If the response is null OR not 200, then return false.
-      if (response.statusCode == null || response.statusCode != 200) {
-        log("Error: ${response.body}");
-        log("Error Code: ${response.statusCode}");
-        return false;
-      }
-
-      return true;
-    } on Exception catch (e) {
-      log("Error: $e");
+    // ? If the response is null OR not 200, then return false.
+    if (response.statusCode == null || response.statusCode != 200) {
+      log("Error: ${response.body}");
+      log("Error Code: ${response.statusCode}");
       return false;
     }
+
+    return true;
   }
 }
