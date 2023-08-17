a50620492e573bb98abd9be4539269f7b8306eb0
Achintya <achintya22052000@gmail.com>
2023-08-17 23:48:08 +0530

    [UI] UI changes

--- vehicle-tracker/vehicle_tracker_app/lib/widgets/login/lang_select_card.dart
+++ vehicle-tracker/vehicle_tracker_app/lib/widgets/login/lang_select_card.dart
@@ -2,39 +2,49 @@ import 'package:digit_components/digit_components.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vehicle_tracker_app/router/routes.dart';
-import 'package:vehicle_tracker_app/util/i18n_translations.dart';
 
 class LangSelectCard extends StatelessWidget {
   const LangSelectCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return DigitCard(
-      child: Column(
-        children: [
-          // * Heading
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("FSSM | Odisha"),
-            ],
-          ),
+    final theme = DigitTheme.instance;
 
-          // * Language Button
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              DigitOutLineButton(label: "Hindi", onPressed: () => Get.updateLocale(const Locale('hi', 'IN'))),
-              DigitOutLineButton(label: "English", onPressed: () => Get.updateLocale(const Locale('en', 'IN'))),
-            ],
-          ),
+    return Padding(
+      padding: theme.verticalMargin,
+      child: DigitCard(
+        child: Column(
+          children: [
+            // * Heading
+            Padding(
+              padding: theme.buttonPadding,
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("FSSM | Odisha"),
+                ],
+              ),
+            ),
 
-          // * Continue Button
-          Padding(
-            padding: DigitTheme.instance.buttonPadding,
-            child: DigitElevatedButton(child: Text(AppTranslation.EMAIL_RESET.tr), onPressed: () => Get.toNamed(LOGIN)),
-          )
-        ],
+            // * Language Button
+            Padding(
+              padding: theme.buttonPadding,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  DigitElevatedButton(child: const Text("Hindi"), onPressed: () {}),
+                  DigitOutLineButton(label: "English", onPressed: () => Get.updateLocale(const Locale('en', 'IN'))),
+                ],
+              ),
+            ),
+
+            // * Continue Button
+            Padding(
+              padding: theme.buttonPadding,
+              child: DigitElevatedButton(child: const Text("Continue"), onPressed: () => Get.toNamed(LOGIN)),
+            )
+          ],
+        ),
       ),
     );
   }
