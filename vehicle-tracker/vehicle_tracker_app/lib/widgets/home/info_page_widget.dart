22643fa5acaae62e3b721d849105a0bde312c560
Achintya <achintya22052000@gmail.com>
2023-08-22 00:38:52 +0530

    [fix] added table widget for better dev exp

--- vehicle-tracker/vehicle_tracker_app/lib/widgets/home/info_page_widget.dart
+++ vehicle-tracker/vehicle_tracker_app/lib/widgets/home/info_page_widget.dart
@@ -2,55 +2,72 @@ import 'package:digit_components/theme/digit_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vehicle_tracker_app/models/home_trip/home_trip_model/home_trip_model.dart';
-import 'package:vehicle_tracker_app/widgets/home/status_info_widget.dart';
 
 import '../../blocs/home/controllers/trip_tracker_controllers.dart';
+import 'status_info_widget.dart';
 
 Widget textColumnWidget(Rx<HomeTripModel> data) => Padding(
       padding: DigitTheme.instance.verticalMargin,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
+      child: Table(
+        columnWidths: const {
+          0: FlexColumnWidth(2),
+          1: FlexColumnWidth(6),
+        },
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          TableRow(
             children: [
               paddedText('Trip ID', bold: true),
-              paddedText('Name', bold: true),
-              paddedText('Vehicle Number', bold: true),
-              paddedText("Pickup Location", bold: true),
-              paddedText("Drop Location", bold: true),
-              paddedText("Date", bold: true),
-              paddedText("Status", bold: true)
+              paddedText(data.value.id),
             ],
           ),
-          const Spacer(),
-          Expanded(
-            flex: 6,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                paddedText(data.value.id),
-                paddedText(data.value.operator.name),
-                paddedText(data.value.operator.vehicleNumber),
-                paddedText(data.value.routeId),
-                paddedText(data.value.routeId),
-                paddedText(data.value.plannedStartTime ?? ""),
-                GetBuilder<TripControllers>(
-                  id: data.value.id,
-                  builder: (tripControllers) => statusInfoWidget(data.value.status),
-                ),
-              ],
-            ),
+          TableRow(
+            children: [
+              paddedText('Name', bold: true),
+              paddedText(data.value.operator.name),
+            ],
+          ),
+          TableRow(
+            children: [
+              paddedText('Vehicle Number', bold: true),
+              paddedText(data.value.operator.vehicleNumber),
+            ],
+          ),
+          TableRow(
+            children: [
+              paddedText("Pickup Location", bold: true),
+              paddedText(data.value.routeId),
+            ],
+          ),
+          TableRow(
+            children: [
+              paddedText("Drop Location", bold: true),
+              paddedText(data.value.routeId),
+            ],
+          ),
+          TableRow(
+            children: [
+              paddedText("Date", bold: true),
+              paddedText(data.value.plannedStartTime ?? ""),
+            ],
+          ),
+          TableRow(
+            children: [
+              paddedText("Status", bold: true),
+              GetBuilder<TripControllers>(
+                id: data.value.id,
+                builder: (tripControllers) => statusInfoWidget(data.value.status),
+              ),
+            ],
           ),
         ],
       ),
     );
 
 Widget paddedText(String value, {bool bold = false}) => Padding(
-      padding: DigitTheme.instance.verticalMargin,
+      padding: DigitTheme.instance.verticalMargin * 0.65,
       child: Text(
         value,
-        maxLines: 1,
+        maxLines: 2,
         overflow: TextOverflow.ellipsis,
         style: bold ? const TextStyle(fontWeight: FontWeight.bold) : null,
       ),
