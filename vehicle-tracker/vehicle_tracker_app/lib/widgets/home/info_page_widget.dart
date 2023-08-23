2c2d3cc61a70aa637b66276db5bbf0416c37328c
Achintya <achintya22052000@gmail.com>
2023-08-23 12:57:08 +0530

    [fix] added unstructured padding

--- vehicle-tracker/vehicle_tracker_app/lib/widgets/home/info_page_widget.dart
+++ vehicle-tracker/vehicle_tracker_app/lib/widgets/home/info_page_widget.dart
@@ -6,61 +6,91 @@ import 'package:vehicle_tracker_app/models/home_trip/home_trip_model/home_trip_m
 import '../../blocs/home/controllers/trip_tracker_controllers.dart';
 import 'status_info_widget.dart';
 
-Widget textColumnWidget(Rx<HomeTripModel> data) => Padding(
-      padding: DigitTheme.instance.verticalMargin,
-      child: Table(
-        columnWidths: const {
-          0: FlexColumnWidth(2),
-          1: FlexColumnWidth(6),
-        },
-        children: [
-          TableRow(
-            children: [
-              paddedText('Trip ID', bold: true),
-              paddedText(data.value.id),
-            ],
-          ),
-          TableRow(
-            children: [
-              paddedText('Name', bold: true),
-              paddedText(data.value.operator.name),
-            ],
-          ),
-          TableRow(
-            children: [
-              paddedText('Vehicle Number', bold: true),
-              paddedText(data.value.operator.vehicleNumber),
-            ],
-          ),
-          TableRow(
-            children: [
-              paddedText("Pickup Location", bold: true),
-              paddedText(data.value.routeId),
-            ],
-          ),
-          TableRow(
-            children: [
-              paddedText("Drop Location", bold: true),
-              paddedText(data.value.routeId),
-            ],
-          ),
-          TableRow(
-            children: [
-              paddedText("Date", bold: true),
-              paddedText(data.value.plannedStartTime ?? ""),
-            ],
-          ),
-          TableRow(
-            children: [
-              paddedText("Status", bold: true),
-              GetBuilder<TripControllers>(
-                id: data.value.id,
-                builder: (tripControllers) => statusInfoWidget(data.value.status),
-              ),
-            ],
-          ),
-        ],
-      ),
+Widget textColumnWidget(Rx<HomeTripModel> data) {
+  final textTheme = DigitTheme.instance.mobileTheme.textTheme;
+
+  return Padding(
+    padding: DigitTheme.instance.verticalMargin,
+    child: Column(
+      children: [
+        textRow('Trip ID', data.value.id),
+        textRow("Name", data.value.operator.name),
+        textRow('Vehicle \nNumber', data.value.operator.contactNumber),
+        textRow('Pickup \nLocation', data.value.routeId),
+        textRow('Drop \nLocation', data.value.routeId),
+        textRow('Date', data.value.plannedStartTime ?? ""),
+        Row(
+          children: [
+            paddedText("Status : ", bold: true),
+            GetBuilder<TripControllers>(
+              id: "data.value.id",
+              builder: (tripControllers) => statusInfoWidget(data.value.status),
+            ),
+          ],
+        )
+      ],
+    ),
+    // child: Table(
+    //   columnWidths: const {
+    //     0: FlexColumnWidth(2),
+    //     1: FlexColumnWidth(6),
+    //   },
+    //   children: [
+    //     TableRow(
+    //       children: [
+    //         paddedText('Trip ID', bold: true),
+    //         paddedText(data.value.id),
+    //       ],
+    //     ),
+    //     TableRow(
+    //       children: [
+    //         paddedText('Name', bold: true),
+    //         paddedText(data.value.operator.name),
+    //       ],
+    //     ),
+    //     TableRow(
+    //       children: [
+    //         paddedText('Vehicle Number', bold: true),
+    //         paddedText(data.value.operator.vehicleNumber),
+    //       ],
+    //     ),
+    //     TableRow(
+    //       children: [
+    //         paddedText("Pickup Location", bold: true),
+    //         paddedText(data.value.routeId),
+    //       ],
+    //     ),
+    //     TableRow(
+    //       children: [
+    //         paddedText("Drop Location", bold: true),
+    //         paddedText(data.value.routeId),
+    //       ],
+    //     ),
+    //     TableRow(
+    //       children: [
+    //         paddedText("Date", bold: true),
+    //         paddedText(data.value.plannedStartTime ?? ""),
+    //       ],
+    //     ),
+    //     TableRow(
+    //       children: [
+    //         paddedText("Status", bold: true),
+    //         GetBuilder<TripControllers>(
+    //           id: data.value.id,
+    //           builder: (tripControllers) => statusInfoWidget(data.value.status),
+    //         ),
+    //       ],
+    //     ),
+    //   ],
+    // ),
+  );
+}
+
+Widget textRow(String key, String value) => Row(
+      children: [
+        paddedText("$key : ", bold: true),
+        Expanded(child: paddedText(value)),
+      ],
     );
 
 Widget paddedText(String value, {bool bold = false}) => Padding(
