1b7127dd3bd978d273795ad039a13e582f4f129d
Venkata Satwik <153486396+Satwik-egov@users.noreply.github.com>
2024-04-22 14:31:14 +0530

    Updated constants.dart

--- starterPack/lib/utils/constants.dart
+++ starterPack/lib/utils/constants.dart
@@ -54,33 +54,4 @@ class RequestInfoData {
   static String? authToken;
 }
 
-class EntityPlurals {
-  static String getPluralForEntityName(String entity) {
-    switch (entity) {
-      case 'Beneficiary':
-        return 'Beneficiaries';
-      case 'ProjectBeneficiary':
-        return 'ProjectBeneficiaries';
-      case 'Address':
-        return 'Addresses';
-      case 'Facility':
-        return 'Facilities';
-      case 'ProjectFacility':
-        return 'ProjectFacilities';
-      case 'Project':
-        return 'Projects';
-      case 'Stock':
-        return 'Stock';
-      case 'StockReconciliation':
-        return 'StockReconciliation';
-      case 'User':
-        return 'user';
-      case 'AttendanceRegister':
-        return 'attendanceRegister';
-      case 'Attendance':
-        return 'attendance';
-      default:
-        return '${entity}s';
-    }
-  }
-}
+
