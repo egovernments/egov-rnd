f598749b55f736934ddf256ca29ca35372ae94db
NabeelAyubee <nayubi7@gmail.com>
2025-10-08 11:54:02 +0530

    initial commit task toolkit

---
+++ TaskToolkit/src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { AuthProvider } from './context/AuthContext'
+import './App.css'
+import HelloWorld from './screens/HelloWorld'
+import Login from './screens/Login'
+import ProtectedRoute from './components/ProtectedRoute'
+
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
+
+function App() {
+  return (
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <HelloWorld />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </GoogleOAuthProvider>
+  )
+}
+
+export default App
