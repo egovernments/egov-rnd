7059a5f44dbd299ffff32ff05aacf4a87ad62c16
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2025-10-08 14:36:31 +0530

    Updated issues

--- TaskToolkit/vite.config.js
+++ TaskToolkit/vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode, command }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react()],
+    base: command === 'serve' ? '/' : '/egov-rnd/',
     server: {
       proxy: {
         "/api/jira": {
