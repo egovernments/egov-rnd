6c816b47a734bb6b1d84a736fbb074ddaf4d85d0
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2025-10-08 12:26:29 +0530

    updated package for gh pages

--- TaskToolkit/vite.config.js
+++ TaskToolkit/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/egov-rnd/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
